924e31c5f285873437fafe6c9ae193d898fb5456
MIGEL ARACHCHIGE Damien <Damien.Migel_Arachchige@bordeaux-inp.fr>
2019-11-22 16:07:26 +0100

    periodogramme

---
+++ main.m
@@ -0,0 +1,44 @@
+close all
+clear
+clc
+
+addpath('projet_signal-master/')
+addpath('periodogramme/')
+s1 = load('fcno03fz.mat');
+s2 = load('fcno04fz.mat');
+
+sigma = 1;
+n = 10000;
+% bruit = @(n,sigma) randn(1,n)*sigma;
+
+bruit = randn(1,n)*sigma;
+
+%% autocorrelation 
+% theorique  
+autocor_theo = zeros(1,n);
+autocor_theo(round(n/2)) = sigma;
+
+% estimateur biased
+autocor_biased = xcorr(bruit,'biased');
+
+% estimateur unbiased
+autocor_unbiased = xcorr(bruit,'unbiased');
+
+%% Affichage
+% figure,plot(autocor_theo);
+% figure,plot(autocor_biased);
+% figure,plot(autocor_unbiased);
+
+
+%% spectre de puissance 
+% sdp = fftshift(abs(fft(bruit))).^2/n;
+% sdp1=abs(fft(autocor_theo));
+% figure,plot(sdp);
+% figure,plot(sdp1);
+
+%% Periodogramme
+pdaniel = getpDaniel(bruit,n/100);
+pbarlett= getpBarlett(bruit,n/10);
+
+figure,plot(pdaniel);
+figure,plot(pbarlett);
---
+++ periodogramme/getpDaniel.m
@@ -0,0 +1,12 @@
+function [ densite ] = getpDaniel(signal,n)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+    autocor_unbiased = fftshift(abs(fft(signal))).^2/length(signal);
+    bis1 = signal(end-n+1:end);
+    bis2 = signal(1:n);
+    
+    densite = conv([bis1 autocor_unbiased bis2], ones(1,n))/n;
+    densite = densite(n+n/2:end-3*n/2);
+
+end
+
