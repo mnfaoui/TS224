b73726df4d420173cb4d79c4a3c75ce44ab636ad
Damien MIGEL ARACHCHIGE <dmigelarach@enseirb-matmeca.fr>
2019-12-14 17:32:49 +0100

    update de l'algo

--- fonction/getTrame.m
+++ fonction/getTrame.m
@@ -1,14 +1,26 @@
-function [trame] = getTrame(window,signal)
+function [trameSansFenetre,trameAvecFenetre,signal] = getTrame(window,signal)
 %UNTITLED Summary of this function goes here
 %   exemple : [trame] = getTrame(window(@hamming,lenWindow),signal)
-lenWindow = length(window);
 
-A = reshape(signal(1:n*lenWindow),lenWindow,n);
-B = reshape(signal1(1:n1*lenWindow),lenWindow,n1);
+lenWindow = length(window);
+len       = length(signal);
+n         = floor(len/lenWindow);
+signal    = signal(1:n*lenWindow);
+
+signalbis = signal(lenWindow/2+1:end);
+lenbis = length(signalbis);
+n1 = floor(lenbis/lenWindow);
+signalbis = signalbis(1:n1*lenWindow);
+
+A = reshape(signal,lenWindow,n);
+B = reshape(signalbis,lenWindow,n1);
 
 C = zeros(lenWindow,n1+n);
 C(:,1:2:end) = A;
-C(:,2:2:end) = B; 
+C(:,2:2:end) = B;
 trame =C.*repmat(window,1,n1+n) ;
+
+trameSansFenetre = C;
+trameAvecFenetre = trame;
 end
 
--- partie2_5.m
+++ partie2_5.m
@@ -1,83 +1,86 @@
 close all
-clear 
-clc 
+clear
+clc
 
-
-%% Declaration de variable 
-Fe=8e3;
+%% Constantes
 s=load('./projet_signal-master/fcno04fz');
 s=s.fcno04fz;
-SNR = 0.25;
-lenWindow = 25e-3*Fe;  %% un signal de parole peut être modelise comme un signal quaso-stationnaire sur un intervalle de temps de 25 ms
-L = 190;%125
-threshold  = 0.5e5;
+Fe=8e3;
+
+%% Declaration de variable
+
+SNR       = 5;
+lenWindow = 15e-3*Fe;  %% un signal de parole peut être modelise comme un signal quaso-stationnaire sur un intervalle de temps de 25 ms
+L         = 100;%125
+threshold = 0.8e4;
+windows   = window(@hamming,lenWindow);
+
 %% Bruit
 Vs  = (s'*s)/length(s);
 Vb  = Vs/SNR;
 signal = s + sqrt(Vb)*randn(size(s));
 
 %% Fenetrage
+[trameSansFenetre,trameAvecFenetre,signal] = getTrame(windows,signal);
 
-len       = length(signal);
-n         = floor(len/lenWindow);
-signal    = signal(1:n*lenWindow);
-
-signalbis = signal(lenWindow/2+1:end);
-lenbis = length(signalbis);
-n1 = floor(lenbis/lenWindow);
-signalbis = signalbis(1:n1*lenWindow);
-
-A = reshape(signal,lenWindow,n);
-B = reshape(signalbis,lenWindow,n1);
-
-C = zeros(lenWindow,n1+n);
-C(:,1:2:end) = A;
-C(:,2:2:end) = B; 
-trame =C.*repmat(window(@hamming,lenWindow),1,n1+n) ;
+%
 
 %% Hankel
-trame_rcv = trame;
-N = length(trame(:,1));
-M = N-L+1;
-tmptest =zeros(1,size(trame_rcv,2));
 
-for nbTrame=1:size(trame_rcv,2)
-        H = hankel( trame(1:L,nbTrame), trame(L:end,nbTrame));
-        [U,S,V] = svd(H);
-        dia = diag(S);
-        
-        tmptest(nbTrame) = max(S(:));
-%         figure(1)
-%         plot(tmp);
-%         figure(2)
-%         plot(S)
-%         [~,i] = max(dia(1:end-1)-dia(2:end));
-%         X=S.*(S>S(i,i));
-        if(tmptest(nbTrame)>threshold)
-            tmp = dia(1:end-1)-dia(2:end);
-            [~,i] = max(dia(3:end)-2*dia(2:end-1)+dia(1:end-2));
-%             i = i+2;
-            X=S.*(S>S(i,i));
-        else
-            X = 0*S;
-        end
-        Hbis = U*X*V';
-        
-        a = Hbis;
-        [m,n] = size(a);
-        idx = hankel(1:m,m:(n-1)+m);
-        out = accumarray(idx(:),a(:))./[(1:min(n,m)),20*ones(1,abs(n-m)),(min(n,m)-1:-1:1)]';
-        %%tramebis=[Hbis(:,1)', Hbis(end,2:end)];
-%         sum(out == trame(:,nbTrame))
-        trame(:,nbTrame)= out;
+trame_rcv = trameAvecFenetre;
+trame_new = zeros(size(trame_rcv));
+nbTrame   = size(trame_rcv,2);
+N = size(trame_rcv(:,1),1);
+
+listValeurSing =zeros(1,size(trame_rcv,2));
+M = N+1-L;
+
+listU = zeros(L,L,nbTrame);
+listS = zeros(L,M,nbTrame);
+listV = zeros(M,M,nbTrame);
+
+for noTrame=1:size(trame_rcv,2)
+    H = hankel( trame_rcv(1:L,noTrame), trame_rcv(L:end,noTrame));
+    [U,S,V] = svd(H);
+    
+    listU(:,:,noTrame) = U;
+    listS(:,:,noTrame) = S;
+    listV(:,:,noTrame) = V;
+    
+    dia = diag(S);
+    listValeurSing(noTrame) = max(S(:));
+    
+    i = 7;
+    if(S(1,1)>threshold)
+%         tmp = dia(1:end-1)-dia(2:end);
+%         [~,i] = max(dia(3:end)-2*dia(2:end-1)+dia(1:end-2));
+%         i = i+2;
+          
+        X=S.*(S>S(i,i));
+    else
+        X=S/1000;
+    end
+    
+    Hbis = U*X*V';
+    
+    a     = Hbis;
+    [m,n] = size(a);
+    idx   = hankel(1:m,m:(n-1)+m);
+    out   = accumarray(idx(:),a(:),[],@mean);
+   
+    trame_new(:,noTrame)= out;
+%     plot(out);
+
 end
 
-%% Reconstitution du signal 
+%% Reconstitution du signal
 % s_k(t) = x(t)W(t-kT);
 % s_{k+1}(t) = x(t)W(t-(k+1)T);
 % x(t) = (s_k(t)+s_{k+1}(t)) / ( W(t-kT)+W(t-(k+1)T) );
 
 % trame = trame(:,1:end-1);
+% trame = trame_new;
+trame = trame_new;
 new_Signal = trame(:,1:2:end);
 new_Signal = new_Signal(:);
 
@@ -85,24 +88,26 @@ new_Signal_Bis = trame(:,2:2:end);
 new_Signal_Bis = new_Signal_Bis(:);
 
 new_Signal(size(trame,1)/2+1:end-size(trame,1)/2) ...
-    = new_Signal(size(trame,1)/2 +1:end-size(trame,1)/2) + new_Signal_Bis(1:end);
-W = window(@hamming,size(trame,1)) ;
+    =  new_Signal(size(trame,1)/2 +1:end-size(trame,1)/2)...
+     + new_Signal_Bis(1:end);
+
+W = windows;
 W = repmat(W,1,length(new_Signal)/length(W)) ;
 W = W(:);
 W(size(trame,1)/2+1:end-size(trame,1)/2) ...
     = W(size(trame,1)/2 +1:end-size(trame,1)/2) + W(1:end-lenWindow);
 
-new_Signal = new_Signal./W;
+signal_new = new_Signal./W;
+
 
 figure,hold on
-plot(new_Signal);
-plot(s)
-plot(new_Signal);
-
-figure,hold on
-plot(new_Signal);
 plot(signal);
-% figure
+plot(signal_new);
+
+figure,hold on
+plot(signal_new);
+plot(signal);
+
 
 
 figure
